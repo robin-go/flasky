0f530e03b9794aa4b0aa09286a32c522d8529b59
robin <doubin@yuntongxun.com>
2018-03-26 19:11:06 +0800

    模板

--- hello.py
+++ hello.py
@@ -1,21 +1,23 @@
 from flask import Flask
 from flask import request, current_app
-from flask import make_response, redirect, abort
+from flask import make_response, redirect, abort, render_template
 
-from flask.ext.script import Manager
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello %s</h1>' % name
+    return render_template('user.html', name=name)
 
 
 @app.route('/ua/')
