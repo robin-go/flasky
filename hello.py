e4ec35e96c75a94462837d05e44dca96d0eabffc
robin <doubin@yuntongxun.com>
2018-03-27 19:01:44 +0800

    模板继承

--- hello.py
+++ hello.py
@@ -20,6 +20,16 @@ def user(name):
     return render_template('user.html', name=name)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
 @app.route('/ua/')
 def get_ua():
     ua = request.headers.get('User-Agent')
